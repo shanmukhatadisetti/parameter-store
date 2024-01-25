a5c654fcbb0a0ca5660d87eaa02305d472f01360
shanmukh <shanmukhatadisetti@gmail.com>
2024-01-25 23:22:13 +0530

    Expense Script

--- main.tf
+++ main.tf
@@ -5,12 +5,12 @@ resource "aws_ssm_parameter" "parameters" {
   value = var.parameters[count.index].value
 }
 
-variable "parameters" {
-  default = [
-    {name = "prod.rds.master_username" , value = "Expense" , type = "String"},
-    {name = "prod.rds.master_password" , value = "Expenseprod1234" , type = "SecureString"},
-    {name = "prod.rds.endpoint" , value = "prod-mysql.cluster-cnjbzccd9deq.us-east-1.rds.amazonaws.com" , type = "String"},
-    {name = "prod.expense.frontend.backend_url" , value = "http://backend-prod.autonagar.in/" , type = "String"},
+#variable "parameters" {
+#  default = [
+#    {name = "prod.rds.master_username" , value = "Expense" , type = "String"},
+#    {name = "prod.rds.master_password" , value = "Expenseprod1234" , type = "SecureString"},
+#    {name = "prod.rds.endpoint" , value = "prod-mysql.cluster-cnjbzccd9deq.us-east-1.rds.amazonaws.com" , type = "String"},
+#    {name = "prod.expense.frontend.backend_url" , value = "http://backend-prod.autonagar.in/" , type = "String"},
 
     #dev env
 
